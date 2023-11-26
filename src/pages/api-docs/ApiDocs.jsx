74c4302210d4784019b4e831c5e42b907dd7225f
Reza IF <rezaif79.ri@gmail.com>
2023-11-26 19:57:04 +0700

    feat: add api docs page
    Api docs page linked with local spec json file on assets folder

--- src/pages/api-docs/ApiDocs.jsx
+++ src/pages/api-docs/ApiDocs.jsx
@@ -1,10 +1,16 @@
 import SwaggerUI from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css";
+import "./ApiDocs.css"
+import petshopApiDoc from "../../assets/petshop-swagger-api-docs.json"
+
 
 export default function ApiDocs() {
+  const spec = {
+    swagger: "2.0"
+  }
   return (
-    <div className="App bg-white h-full pt-5 overflow-auto">
-     <SwaggerUI url="https://petstore.swagger.io/v2/swagger.json" />
+    <div className="bg-white h-full pt-5 overflow-auto">
+     <SwaggerUI spec={petshopApiDoc}/>
     </div>
   )
 }
